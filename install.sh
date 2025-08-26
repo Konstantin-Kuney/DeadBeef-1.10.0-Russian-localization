4b4638c5ed72830d49869f54c5eb9546c807a246
Konstantin-Kuney <kmkuney@gmail.com>
2025-08-26 14:44:21 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# script by Konstantin Kuney 11.11.2024
+# script by Konstantin Kuney 26.08.2025
 
 /usr/bin/mkdir -p /opt/deadbeef/share/locale/ru/LC_MESSAGES/
 /usr/bin/cp -f ru.mo /opt/deadbeef/share/locale/ru/LC_MESSAGES/deadbeef.mo
-/usr/bin/cat translators.txt > /opt/deadbeef/share/doc/deadbeef/translators.txt
+/usr/bin/cat translators_ru.txt > /opt/deadbeef/share/doc/deadbeef/translators.txt
+/usr/bin/cat about_ru.txt > /opt/deadbeef/share/doc/deadbeef/about.txt
+/usr/bin/cat ChangeLog_DeaDBeeF > /opt/deadbeef/share/doc/deadbeef/ChangeLog
+/usr/bin/cp -f help_ru.txt /opt/deadbeef/share/doc/deadbeef/help.ru.txt
 
--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-# script by Konstantin Kuney 11.11.2024
+# script by Konstantin Kuney 26.08.2025
 
 /usr/bin/rm -rf /opt/deadbeef/share/locale/ru/
 /usr/bin/cat translators_orig.txt > /opt/deadbeef/share/doc/deadbeef/translators.txt
+/usr/bin/cat about_orig.txt > /opt/deadbeef/share/doc/deadbeef/about.txt
+/usr/bin/cat ChangeLog_DeaDBeeF_orig > /opt/deadbeef/share/doc/deadbeef/ChangeLog
+/usr/bin/rm -f /opt/deadbeef/share/doc/deadbeef/help.ru.txt
 
